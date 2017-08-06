53171fdbbbb1e92536eec045fa3c5d6b19a81cfe
ShubhangiSharma1197 <30749783+ShubhangiSharma1197@users.noreply.github.com>
2017-08-06 16:39:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,13 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="New Value";
-
+var marginLeft=0;
+function moveRight(){
+    marginLeft= marginLeft+5;
+    img.style.marginLeft=marginLeft + 'px';
+}
 var img=document.getElementById('madi');
 img.onclick = function() {
-    img.style.marginLeft='100px';
+    //img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,50);
 };
