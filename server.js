9482a487bc056e96add5c287e6e994afd33a94fe
ShubhangiSharma1197 <30749783+ShubhangiSharma1197@users.noreply.github.com>
2017-08-13 18:36:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,9 +132,9 @@ app.get('/submit-name', function(req,res){ // /submit-name?name=xxxx
    res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName',function(req,res){
+app.get('/article/:articleName',function(req,res){
  var articleName = req.params.articleName;
- pool.query("SELECT * from article WHERE title ='"+req.params.articleName +"'", function(err,result){
+ pool.query("SELECT * from article WHERE title =$1",[req.params.articleName], function(err,result){
  if(err){
     res.status(500).send(err.toString());
 }else{
