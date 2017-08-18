f957a8836dcc908133ff337ec1d12ffe8847ec73
ShubhangiSharma1197 <30749783+ShubhangiSharma1197@users.noreply.github.com>
2017-08-18 20:58:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ var title=data.title;
 var heading=data.heading;
 var date=data.date;
 var content=data.content;
+var crypto = require('crypto');
 var htmlTemplate = `
 
 <html>
@@ -107,7 +108,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool=new Pool(config);
-
+function hash(input){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+   var hashedString = hash(req.params.input, 'this is some random string');
+   res.send(hashedString);
+});
 
 app.get('/test-db',function(req,res){
 pool.query('SELECT * FROM test',function(err,result){
