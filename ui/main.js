65c936b39bd33c7d2b3810f37c1b3d5cd5d37e60
ShubhangiSharma1197 <30749783+ShubhangiSharma1197@users.noreply.github.com>
2017-08-29 17:56:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,4 @@
-`console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML="New Value";
-var marginLeft=0;
-function moveRight(){
-    marginLeft= marginLeft+5;
-    img.style.marginLeft=marginLeft + 'px';
-}
-var img=document.getElementById('madi');
-img.onclick = function() {
-    //img.style.marginLeft='100px';
-    var interval = setInterval(moveRight,50);
-};`;
 
-//counter code
-/*
-var button=document.getElementById('counter');
-
-button.onclick=function()
-{
-    var request= new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE){
-           if(request.status === 200){
-               var counter = request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-           }        
-        }
-    };
-    request.open('GET','http://sharmaneeraj.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-    
-   
-};
-*/
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
 var request= new XMLHttpRequest();
@@ -60,4 +24,28 @@ request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username:username,password:password}));
 };
 
+
+//counter code
+/*
+var button=document.getElementById('counter');
+
+button.onclick=function()
+{
+    var request= new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+           if(request.status === 200){
+               var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+           }        
+        }
+    };
+    request.open('GET','http://sharmaneeraj.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
+   
+};
+*/
 
