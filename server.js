8acd50792dfe449fffe76a57dfb016163d4bd273
ShubhangiSharma1197 <30749783+ShubhangiSharma1197@users.noreply.github.com>
2017-08-29 18:49:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -176,6 +176,10 @@ app.get('/check-login', function(req,res){
    }
    
 });
+app.get('/logout',function(req,res){
+   delete req.session.auth;
+   res.send('Logged Out');
+});
 
 app.get('/test-db',function(req,res){
 pool.query('SELECT * FROM test',function(err,result){
