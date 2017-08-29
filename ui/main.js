f8ea52d4a4461f86ae6ee0c2d25efc6e917b6a55
ShubhangiSharma1197 <30749783+ShubhangiSharma1197@users.noreply.github.com>
2017-08-29 17:39:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,49 +34,29 @@ button.onclick=function()
    
 };
 
-/*var submit=document.getElementById('submit_btn');
+var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
 var request= new XMLHttpRequest();
     request.onreadystatechange = function(){
         if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
            if(request.status === 200){
-            var names = request.responseText;
-            names = JSON.parse(names);
-         var list='';
-  for(var i=0;i<names.length ;i++)
-  {
-      list += '<li>'+names[i] + '</li>';
-  }
-  var ul= document.getElementById('namelist');
-  ul.innerHTML = list;   
+            console.log('user logged in');
+            alert('Logged in successfully');
+           }else if(request.status === 403){
+               alert('invalid username/password');
+           }else if(request.status === 500){
+               alert('Something went wrong on the server');
            }        
         }
     };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    request.open('POST','http://localhost:8080/create-user',true);
+   console.log(username);
+   console.log(password);
+  request.open('POST','http://sharmaneeraj.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type', 'application/json');  
     request.send(JSON.stringify({username:username,password:password}));
-    
-    var nameInput= document.getElementById('name');
-var name=nameInput.value;
-  request.open('GET','http://sharmaneeraj.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
-};*/
-var submitButton = document.getElementById('submit_btn');
-submitButton.onclick = function(){
-    var request = new XMLHttpRequest();
-
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE && request.status ===200){
-            var data = JSON.parse(request.responseText);
-            console.log(data);
-        }
-    };
-
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-request.open('POST','http://localhost:8080/create-user',true);
-request.setRequestHeader('content-type', 'application/json');
-request.send(JSON.stringify({username:username,password:password}));
 };
+
 
